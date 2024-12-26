db7b21727a9b71eb2d09e8d8ae5d1d217156ae15
nayeem syed <smn7818@gmail.com>
2024-12-26 18:14:20 -0500

    fixed codeQL issues

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Enable debug mode only in development
+    debug_mode = os.environ.get('FLASK_ENV') == 'development'
+    app.run(debug=debug_mode)
